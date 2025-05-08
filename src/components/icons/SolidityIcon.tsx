ab567639a181f6a5a0481888ef7669375c3aae57
Atsuhiro <atcheri@gmail.com>
2025-05-08 22:17:38 +0200

    fix: use types

--- src/components/icons/SolidityIcon.tsx
+++ src/components/icons/SolidityIcon.tsx
@@ -1,9 +1,10 @@
-import { FC } from 'react';
-import { IconProps } from './types';
+import { type FC } from "react";
 
-export const SolidityIcon: FC<IconProps> = ({ active = true, ...rest }) => {
+import { type IconProps } from "./types";
+
+export const SolidityIcon: FC<IconProps> = (props) => {
   return (
-    <svg viewBox="0 0 523.9 812.97" xmlns="http://www.w3.org/2000/svg" width="1611" height="2500" {...rest}>
+    <svg viewBox="0 0 523.9 812.97" xmlns="http://www.w3.org/2000/svg" width="1611" height="2500" {...props}>
       <path d="M391.93 0l-130.7 232.3H0L130.6 0z" opacity=".45" />
       <path d="M261.23 232.3h261.3L391.94 0H130.6z" opacity=".6" />
       <path d="M130.6 464.5l130.63-232.2L130.6 0 0 232.3z" opacity=".8" />
